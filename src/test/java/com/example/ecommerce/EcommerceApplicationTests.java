fbd87232f54998ce9eeb3e821f41ff33ce4ec7cb
venkatapadma <padmaganapathi568@gmail.com>
2026-05-10 12:01:08 +0100

    created test properties for executing test files

--- src/test/java/com/example/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/example/ecommerce/EcommerceApplicationTests.java
@@ -1,10 +1,17 @@
 package com.example.ecommerce;
 
+import com.example.ecommerce.repo.ProductRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcommerceApplicationTests {
+
+    @MockitoBean
+    private ProductRepo productRepo;
 
     @Test
     void contextLoads() {
